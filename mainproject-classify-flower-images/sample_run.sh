c9acf83d1c5f16ea1c43c6b0668d4ca6eae8736b
ubuntu <vittorio.nardone@gmail.com>
2018-12-18 12:26:34 +0100

    Samples updated

--- mainproject-classify-flower-images/sample_run.sh
+++ mainproject-classify-flower-images/sample_run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-python train.py flowers --save_dir checkpoints/ --arch vgg11 --hidden_units 1024,1024 --gpu
-python train.py flowers --save_dir checkpoints/ --arch vgg19 --hidden_units 1024,1024 --gpu
-python train.py flowers --save_dir checkpoints/ --arch vgg16_bn --hidden_units 1024,1024 --gpu
-python train.py flowers --save_dir checkpoints/ --arch densenet121 --gpu
-python train.py flowers --save_dir checkpoints/ --arch densenet201 --gpu
-python train.py flowers --save_dir checkpoints/ --arch alexnet --hidden_units 4096,4096 --gpu
+python train.py flowers --save_dir checkpoints/ --arch vgg16 --hidden_units 1024,1024 --bn --epochs 10
+python train.py flowers --save_dir checkpoints/ --arch densenet201 --gpu --backend pytorch
 
-
-
-python predict.py test_img.jpg checkpoints/cp_densenet121_e7_lr0.05.pth --top_k 5 --category_names cat_to_name.json
+python predict.py testimg.jpg checkpoints/cp_densenet121_e7_lr0.05.pth --top_k 5 --category_names cat_to_name.json
+python predict.py test checkpoints/cp_resnet50_e10_lr0.05.h5 --category_names cat_to_name.json
 
 
 
